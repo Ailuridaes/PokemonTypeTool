56a84bd05fef1add99e4cf662e29bb7fa20f769c
Ailuridaes <ailuridaes@gmail.com>
2017-04-15 21:25:58 -0700

    Refactor TypeChart component

--- src/components/TypeChart.jsx
+++ src/components/TypeChart.jsx
@@ -2,28 +2,26 @@ import React from 'react';
 import MatchupType from './MatchupType'
 
 const TypeChart = (props) => {
+  const layout = [
+    { damage: "quadruple", display: "4x" },
+    { damage: "double", display: "2x" },
+    { damage: "half", display: "1/2x" },
+    { damage: "quarter", display: "1/4x" },
+    { damage: "zero", display: "0x" }
+  ];
+
   return (
     <div>
-      <h3>4x</h3>
-      {props.matchups.quadruple.map(type =>
-        <MatchupType key={type} type={type} />
-      )}
-      <h3>2x</h3>
-      {props.matchups.double.map(type =>
-        <MatchupType key={type} type={type} />
-      )}
-      <h3>1/2x</h3>
-      {props.matchups.half.map(type =>
-        <MatchupType key={type} type={type} />
-      )}
-      <h3>1/4x</h3>
-      {props.matchups.quarter.map(type =>
-        <MatchupType key={type} type={type} />
-      )}
-      <h3>0x</h3>
-      {props.matchups.zero.map(type =>
-        <MatchupType key={type} type={type} />
-      )}
+      {layout.map(d => {
+        return (
+          <div key={d.damage}>
+            <h3>{d.display}</h3>
+            {props.matchups[d.damage].map(type =>
+              <MatchupType key={type} type={type} />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
