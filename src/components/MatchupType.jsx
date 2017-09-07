4aa6f5731d4b2fd6393a7fbed37b7fc9c9c15b46
Ailuridaes <ailuridaes@gmail.com>
2017-09-06 23:05:37 -0700

    Add styling to matchup type indicators

--- src/components/MatchupType.jsx
+++ src/components/MatchupType.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 
 const MatchupType = (props) => {
   return (
-    <div>
+    <div style={getDivStyle(props.type)}>
         {props.type}
     </div>
   );
+};
+
+function getDivStyle(type) {
+  return {
+    background: `var(--${type.toLowerCase()}-color)`,
+    display: "inline-block",
+    width: "58px",
+    lineHeight: "1",
+    margin: "2px",
+    padding: "5px 0",
+    border: "1px solid #aaa",
+    borderRadius: "2px",
+    color: "#fff",
+    fontSize: "10px",
+    textAlign: "center",
+    textShadow: "1px 1px 1px #333",
+    textTransform: "uppercase"
+  }
 };
 
 export default MatchupType;
